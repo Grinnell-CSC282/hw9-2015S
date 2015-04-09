d177d7473bf394d2642ac700cfe3dcebd3f79a14
Kieran Connolly <KieranConnolly@users.noreply.github.com>
2015-04-09 03:47:48 -0500

    Create squareIt.c

---
+++ connolly17/squareIt.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+#include "squareIt.h"
+
+//This is the definition of the squareIt function.
+
+int squareIt (int x)
+{
+  int squared;
+  squared = x * x;
+  return squared;
+}
